aa1c4f669cc1796c0aa2da52571e7a7cb985aa20
Devin Greene <ddg412@gmail.com>
2021-12-03 16:10:21 -0600

    Solved day2 part 1

--- Day2/dive_part1.py
+++ Day2/dive_part1.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 lines = [line.strip() for line in open("input")]  
+horizontal=0
+depth=0
 for line in lines:
+  (direction, delta) = line.split()
+  if direction == "forward":
+    horizontal += int(delta)
+  elif direction == "down":
+    depth += int(delta)
+  else:
+    depth -= int(delta)
+print("h=" + str(horizontal) + ", d=" + str(depth) + ", total=" + str(horizontal*depth))
