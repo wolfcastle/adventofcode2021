82bf698021fa17f98906ae08e14858baa2e65573
Devin Greene <ddg412@gmail.com>
2021-12-03 19:59:46 -0600

    Cleaned up day3, part2

--- Day3/diagnostic_part2.py
+++ Day3/diagnostic_part2.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 
 def findByte(byteList, desiredValueFunction):
-  oxygenByteList = byteList[:]
+  workingByteList = byteList[:]
   bytePosition = 0
-  while len(oxygenByteList) > 1:
-    columns = list(zip(*oxygenByteList))
+  while len(workingByteList) > 1:
+    columns = list(zip(*workingByteList))
     columnSum = sum(columns[bytePosition])
-    average = len(oxygenByteList)/2
-    #print( "sum: " + str(columnSum) + ", average: " + str(average))
+    average = len(workingByteList)/2
     desiredValue = int(desiredValueFunction(columnSum, average)) 
-    oxygenByteList = [x for x in oxygenByteList if x[bytePosition] == desiredValue]
-    #print("MCV: " + str(desiredValue) + ", bitposition: " + str(bytePosition))
-    #print(oxygenByteList)
+    workingByteList = [x for x in workingByteList if x[bytePosition] == desiredValue]
     bytePosition += 1
-  return ''.join(list(map(str,oxygenByteList[0])))
-
+  return int(''.join(list(map(str,workingByteList[0]))),2)
 
 lines = [line.strip() for line in open("input")]  
-
 byteList = [[int(c) for c in line] for line in lines]
 
 oxygenFunc = lambda sum,avg : sum >= avg
 co2Func = lambda sum,avg : sum < avg
 
-oxygen = int(findByte(byteList, oxygenFunc),2)
-co2 = int(findByte(byteList, co2Func),2)
+oxygen = findByte(byteList, oxygenFunc)
+co2 = findByte(byteList, co2Func)
 print( "oxygen: " + str(oxygen) + ", co2: " + str(co2) + ", life support: " + str(oxygen*co2))
-#print( "gamma: " + str(gamma) + ", epsilon: " + str(epsilon) + ", power: " + str(gamma*epsilon))
