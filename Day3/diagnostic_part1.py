f6331ee8ba91f4d1f466a4de0e914897eaf94c05
Devin Greene <ddg412@gmail.com>
2021-12-03 19:10:30 -0600

    Day3 part1 complete

--- Day3/diagnostic_part1.py
+++ Day3/diagnostic_part1.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 lines = [line.strip() for line in open("input")]  
-for line in lines:
+
+byteList = [[int(c) for c in line] for line in lines]
+columns = list(zip(*byteList))
+columnSums = [sum(x) for x in columns]
+average = len(lines)/2
+truths = [x > average for x in columnSums]
+gamma = int(''.join(list(map(str, list(map(int, truths))))),2)
+truths = [x < average for x in columnSums]
+epsilon = int(''.join(list(map(str, list(map(int, truths))))),2)
+
+print( "gamma: " + str(gamma) + ", epsilon: " + str(epsilon) + ", power: " + str(gamma*epsilon))
